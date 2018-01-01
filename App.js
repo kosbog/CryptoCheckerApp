d6d16f810dd5e2fe7b543d1a787a44efa6ade450
kosbog <badykos@gmail.com>
2018-01-01 21:04:07 +0200

    including Redux

--- App.js
+++ App.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
@@ -11,6 +5,9 @@ import {
   Text,
   View
 } from 'react-native';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducers from './src/reducers';
 import Header from './src/components/Header';
 import CoinsList from './src/components/CoinsList';
 
@@ -24,29 +21,12 @@ const instructions = Platform.select({
 export default class App extends Component<{}> {
   render() {
     return (
+      <Provider store={createStore}>
       <View style={{ flex: 1 }}>
         <Header headerText={'Bit Currencies Checker'}></Header>
         <CoinsList />
       </View>
+      </Provider>
     );
   }
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
